c8eea917e17eff0989837eee5d6cb13dabc5e454
Andreev Denis <andreev@Mac-mini.local>
2019-07-11 13:19:24 +0700

    Updates

--- precache-manifest.6e521cbb478d0c15f6b7da0b749e6241.js
+++ precache-manifest.6e521cbb478d0c15f6b7da0b749e6241.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/phuket/static/js/runtime~main.c24c5275.js"
   },
   {
-    "revision": "4368152d942c8d6c37a5",
-    "url": "/phuket/static/js/main.31cfcc9e.chunk.js"
+    "revision": "da6ea2c65c8d6df711f8",
+    "url": "/phuket/static/js/main.43bd53a5.chunk.js"
   },
   {
     "revision": "bdc9870a4b2e1825da8e",
     "url": "/phuket/static/js/2.679d4ae8.chunk.js"
   },
   {
-    "revision": "4368152d942c8d6c37a5",
+    "revision": "da6ea2c65c8d6df711f8",
     "url": "/phuket/static/css/main.f7eb7193.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/phuket/static/css/2.13937ee1.chunk.css"
   },
   {
-    "revision": "3e15854c25fbf09f0feb6a310c3dc890",
+    "revision": "97d9fca87d023be2cf579bc8b98f2833",
     "url": "/phuket/index.html"
   }
 ];
